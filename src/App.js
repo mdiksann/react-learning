8c5c306ae2d7225dba27e09d565b9e276477030e
iksan <mdiksann@gmail.com>
2025-10-10 09:30:13 +0700

    feat: Add Portfolio and Footer pages

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ import Header from "./Components/Header";
 import About from "./Components/About";
 import Contact from "./Components/Contact";
 import Resume from "./Components/Resume";
+import Portfolio from "./Components/Portofio";
+import Footer from "./Components/Footer";
+
 
 class App extends Component {
   constructor(props) {
@@ -45,7 +48,9 @@ class App extends Component {
         <Header data={this.state.resumeData.main} />
         <About data={this.state.resumeData.main}/>
         <Resume data={this.state.resumeData.resume}/>
+        <Portfolio data={this.state.resumeData.portfolio}/>
         <Contact data={this.state.resumeData.main}/>
+        <Footer data={this.state.resumeData.main}/>
       </div>
     )
   }
