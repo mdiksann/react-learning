d1911d423e5a2d00b7795aef3d6e0d1dc2a9aa6c
iksan <mdiksann@gmail.com>
2025-10-12 12:46:43 +0700

    feat: Converted class components to functional components

---
+++ src/Components/Portfolio.js
@@ -0,0 +1,37 @@
+import React from "react";
+import Zmage from "react-zmage";
+import Fade from "react-awesome-reveal";
+
+const Portfolio = ({ data }) => {
+    if (!data) return null;
+
+    const projects = data.projects.map((project, index) => {
+        const projectImage = "images/portfolio/" + project.image;
+
+        return (
+            <div key={index} className="columns portfolio-item">
+                <div className="item-wrap">
+                    <Zmage alt={project.title} src={projectImage} />
+                    <div style={{textAlign: "center"}}>{project.title}</div>
+                </div>
+            </div>
+        );
+    });
+
+    return (
+        <section id="portfolio">
+            <Fade left duration={1000} distance="60px">
+                <div className="row">
+                    <div className="twelve columns collapsed">
+                        <h1>Check out some of my works</h1>
+                        <div id="portfolio-wrapper" className="bgrid-thirds s-bgrid-thirds cf">
+                            {projects}
+                        </div>
+                    </div>
+                </div>
+            </Fade>
+        </section>
+    );
+};
+
+export default Portfolio;
