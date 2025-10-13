c33337cb0b8912ecfa165c79b03f1a31e4ca9706
iksan <mdiksann@gmail.com>
2025-10-13 20:36:55 +0700

    feat: Converted class components to functional components

--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -1,113 +1,107 @@
-import { Component } from "react";
+import React from "react";
 import Slide from "react-awesome-reveal";
 
-class Resume extends Component {
-    getRandomColor() {
-        let letters = "0123456789ABCDEF";
+const Resume = ({ data }) => {
+    if (!data) return null;
+
+    const getRandomColor = () => {
+        const letters = "0123456789ABCDEF";
         let color = "#";
         for (let i = 0; i < 6; i++) {
-          color += letters[Math.floor(Math.random() * 16)];
+            color += letters[Math.floor(Math.random() * 16)];
         }
         return color;
-    }
+    };
 
-    render() {
-        if (!this.props.data) return null;
+    const { skillmessage, education, work, skills } = data;
 
-        const skillmessage = this.props.data.skillmessage;
-        const education = this.props.data.education.map(function (education) {
-            return (
-                <div key={education.school}>
-                    <h3>{education.school}</h3>
-                    <p className="info">
-                        {education.degree} <span>&bull;</span>
-                        <em className="date">{education.graduated}</em>
-                    </p>
-                    <p>{education.description}</p>
-                </div>
-            );
-        });
+    const educationList = education.map((edu) => (
+        <div key={edu.school}>
+            <h3>{edu.school}</h3>
+            <p className="info">
+                {edu.degree} <span>&bull;</span>
+                <em className="date">{edu.graduated}</em>
+            </p>
+            <p>{edu.description}</p>
+        </div>
+    ));
 
-        const work = this.props.data.work.map(function (work) {
-            return (
-                <div key={work.company}>
-                    <h3>{work.company}</h3>
-                    <p className="info">
-                        {work.title}
-                        <span>&bull;</span> <em className="date">{work.years}</em>
-                    </p>
-                    <p>{work.description}</p>
-                </div>
-            );
-        });
+    const workList = work.map((job) => (
+        <div key={job.company}>
+            <h3>{job.company}</h3>
+            <p className="info">
+                {job.title}
+                <span>&bull;</span> <em className="date">{job.years}</em>
+            </p>
+            <p>{job.description}</p>
+        </div>
+    ));
 
-        const skills = this.props.data.skills.map((skills) => {
-            const backgroundColor = this.getRandomColor();
-            const className = "bar-expand " + skills.name.toLowerCase();
-            const width = skills.level;
-
-            return (
-                <li key={skills.name}>
-                    <span style={{ width, backgroundColor}} className={className}></span>
-                    <em>{skills.name}</em>
-                </li>
-            );
-        });
+    const skillsList = skills.map((skill) => {
+        const backgroundColor = getRandomColor();
+        const className = "bar-expand " + skill.name.toLowerCase();
+        const width = skill.level;
 
         return (
-            <section id="resume">
-                <Slide left duration={1300}>
-                    <div className="row education">
-                        <div className="three columns header-col">
-                            <h1>
-                                <span>Education</span>
-                            </h1>
-                        </div>
-
-                        <div className="nine columns main-col">
-                            <div className="row item">
-                                <div className="twelve columns">{education}</div>
-                            </div>
-                        </div>
-                    </div>
-                </Slide>
-
-                <Slide left duration={1300}>
-                    <div className="row work">
-                        <div className="three columns header-col">
-                            <h1>
-                                <span>Work</span>
-                            </h1>
-                        </div>
-
-                        <div className="nine columns main-col">
-                            <div className="row item">
-                                <div className="twelve columns">{work}</div>
-                            </div>
-                        </div>
-                    </div>
-                </Slide>
-
-                <Slide left duration={1300}>
-                    <div className="row skill">
-                        <div className="three columns header-col">
-                            <h1>
-                                <span>Skills</span>
-                            </h1>
-                        </div>
-
-                        <div className="nine columns main-col">
-                            <p>{skillmessage}</p>
-                            <div className="bars">
-                                <ul className="skills">{skills}</ul>
-                            </div>
-                        </div>
-                    </div>
-                </Slide>
-            </section>
+            <li key={skill.name}>
+                <span style={{ width, backgroundColor }} className={className}></span>
+                <em>{skill.name}</em>
+            </li>
         );
+    });
 
-    }
-}
+    return (
+        <section id="resume">
+            <Slide left duration={1300}>
+                <div className="row education">
+                    <div className="three columns header-col">
+                        <h1>
+                            <span>Education</span>
+                        </h1>
+                    </div>
+
+                    <div className="nine columns main-col">
+                        <div className="row item">
+                            <div className="twelve columns">{educationList}</div>
+                        </div>
+                    </div>
+                </div>
+            </Slide>
+
+            <Slide left duration={1300}>
+                <div className="row work">
+                    <div className="three columns header-col">
+                        <h1>
+                            <span>Work</span>
+                        </h1>
+                    </div>
+
+                    <div className="nine columns main-col">
+                        <div className="row item">
+                            <div className="twelve columns">{workList}</div>
+                        </div>
+                    </div>
+                </div>
+            </Slide>
+
+            <Slide left duration={1300}>
+                <div className="row skill">
+                    <div className="three columns header-col">
+                        <h1>
+                            <span>Skills</span>
+                        </h1>
+                    </div>
+
+                    <div className="nine columns main-col">
+                        <p>{skillmessage}</p>
+                        <div className="bars">
+                            <ul className="skills">{skillsList}</ul>
+                        </div>
+                    </div>
+                </div>
+            </Slide>
+        </section>
+    );
+};
 
 export default Resume;
